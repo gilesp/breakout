0b702ca8f6df374d22b55f50cb2543fa37da8dcd
Giles Paterson <giles.paterson@blackpepper.co.uk>
2018-11-26 19:36:36 +0000

    Initial scaffolding

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+	'app': './src/index.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+	contentBase: './dist'
+    },
+    plugins: [
+	new CleanWebpackPlugin(['dist']),
+	new HtmlWebpackPlugin({
+	    title: 'Breakout'
+	}),
+	new CopyWebpackPlugin([
+	    {
+		from: path.resolve(__dirname, 'assets', '**', '*'),
+		to: path.resolve(__dirname, 'dist')
+	    }
+	])
+    ],
+    output: {
+	filename: '[name].bundle.js',
+	path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+	contentBase: path.resolve(__dirname, 'dist'),
+    }
+};
